f239fcc003912a7289e39af20ac2dcf9b7b335a5
Jonny Nguyen <jonny_nguyen@outlook.com>
2022-03-15 18:44:36 +0000

    add kustomize

--- setup/20-alias.sh
+++ setup/20-alias.sh
@@ -2,4 +2,3 @@ echo "alias kns='kubens'" >> ~/.bashrc
 echo "alias kctx='kubectx'" >> ~/.bashrc
 echo "alias kctl='kubectl'" >> ~/.bashrc
 echo "alias kustomize='kubectl kustomize'" >> ~/.bashrc
-echo "cd /wd" >> ~/.bashrc
