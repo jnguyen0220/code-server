ffbcf7747a45c7577bae35f114f310d9bd1685f1
Jonny Nguyen <jonny_nguyen@outlook.com>
2022-03-18 21:46:22 +0000

    update alias

--- setup/30-alias.sh
+++ setup/30-alias.sh
@@ -3,13 +3,13 @@ echo "alias kns='kubens'" >> ~/.bashrc
 echo "alias kctx='kubectx'" >> ~/.bashrc
 echo "alias kctl='kubectl'" >> ~/.bashrc
 echo "alias kustomize='kubectl kustomize'" >> ~/.bashrc
-echo "\x1B[31m \U2605 \x1B[0m node: $(node -v)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m yarn: $(yarn -v)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m aws cli: $(aws --version)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m docker: $(docker -v)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m $(python3 --version)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m $(go version)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m $(rustc --version)" >> ~/version.txt
-echo "\x1B[31m \U2605 \x1B[0m $(kubectl version)" >> ~/version.txt
 echo "cat ~/logo.txt" >> ~/.bashrc
-echo "echo -e $(cat ~/version.txt)" >> ~/.bashrc
+echo "echo 'Installed run times:'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m node - $(node -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m yarn - $(yarn -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m aws cli - $(aws --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m docker - $(docker -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m python - $(python3 --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m go - $(go version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m rust - $(rustc --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m kubectl - $(kubectl version)'\n" >> ~/.bashrc
