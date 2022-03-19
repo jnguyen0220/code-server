7069502e489228ef721903a1bcb47ac74ac48465
Jonny Nguyen <jonny_nguyen@outlook.com>
2022-03-19 15:24:36 +0000

    add yellow color to text and git

--- setup/30-alias.sh
+++ setup/30-alias.sh
@@ -5,12 +5,13 @@ echo "alias kctl='kubectl'" >> ~/.bashrc
 echo "alias kustomize='kubectl kustomize'" >> ~/.bashrc
 echo "cat ~/logo.txt" >> ~/.bashrc
 echo "echo 'Installed run times:'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m node - $(node -v)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m yarn - $(yarn -v)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m aws cli - $(aws --version)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m docker - $(docker -v)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m python - $(python3 --version)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m go - $(go version)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m rust - $(rustc --version)'" >> ~/.bashrc
-echo "echo -e '\x1B[31m \U2605 \x1B[0m kubectl - $(kubectl version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m git \033[0m - $(git --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m node \033[0m - $(node -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m yarn \033[0m - $(yarn -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m aws cli \033[0m - $(aws --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m docker \033[0m - $(docker -v)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m python \033[0m - $(python3 --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m go \033[0m - $(go version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m rust \033[0m - $(rustc --version)'" >> ~/.bashrc
+echo "echo -e '\x1B[31m \U2605 \x1B[0m \033[33;1m kubectl \033[0m - $(kubectl version)'" >> ~/.bashrc
 echo "echo" >> ~/.bashrc
