f174849e93ece02e3d084165360e52aaf8e1173a
Jonny Nguyen <jonny_nguyen@outlook.com>
2022-08-18 02:52:25 +0000

    revert tar flag

--- setup/20-install-nodejs-yarn.sh
+++ setup/20-install-nodejs-yarn.sh
@@ -7,7 +7,7 @@ NODE_HOME=/opt/$NODE_PACKAGE
 
 export PATH=$NODE_HOME/bin:$PATH
 
-curl https://nodejs.org/dist/v$NODE_VERSION/$NODE_PACKAGE.tar.gz | tar -xvzf /usr/local --strip-components=1 --no-same-owner
+curl https://nodejs.org/dist/v$NODE_VERSION/$NODE_PACKAGE.tar.gz | tar -xzC /usr/local --strip-components=1 --no-same-owner
 ln -s /usr/local/bin/node /usr/local/bin/nodejs 
 
 node --version
